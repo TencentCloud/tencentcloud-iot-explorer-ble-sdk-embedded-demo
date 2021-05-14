68c3b504d53c793d111a7ed8aa421630c4ecd7b7
esma <esma@tencent.com>
2021-05-14 18:22:36 +0800

    add nrf52832 demo

---
+++ qcloud-iot-ble-nrf52832/pca10040/s132/arm5_no_packs/RTE/_nrf52832_xxaa/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'LLSync_Nrf52832' 
+ * Target:  'nrf52832_xxaa' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "nrf.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ qcloud-iot-ble-nrf52832/readme.md
@@ -0,0 +1,4 @@
+## 说明
+
+`LLSync`标准蓝牙功能示例程序，请参考[蓝牙设备接入指引](https://cloud.tencent.com/document/product/1081/50969)
+
