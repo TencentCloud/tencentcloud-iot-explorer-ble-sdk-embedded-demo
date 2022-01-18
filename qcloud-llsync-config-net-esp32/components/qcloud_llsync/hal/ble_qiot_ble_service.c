44c429a4e3f9754e3f96861f68b81f164478b052
Genie-bj <real_huwei@126.com>
2022-01-18 20:55:42 +0800

    feat: update config net demo.

--- qcloud-llsync-config-net-esp32/components/qcloud_llsync/hal/ble_qiot_ble_service.c
+++ qcloud-llsync-config-net-esp32/components/qcloud_llsync/hal/ble_qiot_ble_service.c
@@ -397,7 +397,9 @@ static void gatts_profile_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_
         case ESP_GATTS_DISCONNECT_EVT:
             ESP_LOGI(LLSYNC_LOG_TAG, "ESP_GATTS_DISCONNECT_EVT, reason = 0x%x", param->disconnect.reason);
             ble_gap_disconnect_cb();
+            #if !BLE_QIOT_LLSYNC_CONFIG_NET
             ble_qiot_advertising_start();
+            #endif
             break;
         case ESP_GATTS_CREAT_ATTR_TAB_EVT: {
             if (param->add_attr_tab.status != ESP_GATT_OK) {
